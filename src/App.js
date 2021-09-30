eda40552bb84dc76b6cec60774c00a134ae142bd
jeslyn <jeslyns42@gmail.com>
2021-09-30 10:44:46 +0400

    created project with create react app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import HomePage from './homepage.js';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
