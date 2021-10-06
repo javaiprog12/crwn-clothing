805186de93ae722532afa145e1b7e54913e7f9e5
jeslyn <jeslyns42@gmail.com>
2021-10-06 12:39:42 +0400

    adding header,svg file

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import HomePage from './pages/homepage/homepage.js';
 import React from 'react';
 import {Route,Switch} from 'react-router-dom';
 import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
 
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component = {HomePage}></Route>
         <Route path='/shop' component = {ShopPage}></Route>
