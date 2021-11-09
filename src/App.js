04baf92a25d9da23f91f3d45a8fd57e195de4a29
jeslyn <jeslyns42@gmail.com>
2021-11-09 17:18:56 +0400

    user types

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.js';
 import React from 'react';
-import {Route,Switch} from 'react-router-dom';
+import {Route,Switch,Redirect} from 'react-router-dom';
 import ShopPage from './pages/shop/shop';
 import Header from './components/header/header';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
@@ -54,7 +54,12 @@ class App extends React.Component{
         <Switch>
           <Route exact path='/' component = {HomePage}></Route>
           <Route path='/shop' component = {ShopPage}></Route>
-          <Route path='/signin' component = {SignInAndSignUpPage}></Route>
+          <Route exact path='/signin' render = {() => 
+                  this.props.currentUser?(
+                  <Redirect to='/'/>
+                  ):
+                  <SignInAndSignUpPage/>
+                  }></Route>
         </Switch>
       </div>
     );
@@ -62,8 +67,12 @@ class App extends React.Component{
 
 }
 
+const mapStateToProps = ({user}) =>({
+  currentUser:user.currentUser
+})
+
 const mapDispatchToProps = dispatch =>({
  setCurrentUser:user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
